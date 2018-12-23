0ac50f14650f54269435cca74d00de543a97fe85
James Price <j.price@bristol.ac.uk>
2018-12-23 17:46:28 +0000

    Break down results by classification

--- summary.sh
+++ summary.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-echo "-----------------------------------------------"
-passed=0
-total=0
+print_line='{printf "%-24s: %6d / %6d -> %5.1f%%\n", $1, $2, $3, 100.0*$2/$3}'
+
+echo "---------------------------------------------------"
 for group in results/*
 do
   group=${group#results/}
-  _passed=`wc -l results/$group/PASS | awk '{print $1}'`
-  _total=`wc -l results/$group/{PASS,FAIL,WARN,NOSUPPORT,CRASH,TIMEOUT} | \
+  passed=`wc -l results/$group/PASS | awk '{print $1}'`
+  total=`wc -l results/$group/{PASS,FAIL,WARN,NOSUPPORT,CRASH,TIMEOUT} | \
     tail -n 1 | awk '{print $1}'`
-  echo $group $_passed $_total | \
-    awk '{printf "%-20s: %6d / %6d -> %5.1f%%\n", $1, $2, $3, 100.0*$2/$3}'
-  let passed+=$_passed
-  let total+=$_total
+  echo $group $passed $total | awk "$print_line"
 done
 
-echo "-----------------------------------------------"
-echo $passed $total | \
-  awk '{printf "Total tests passed  : %6d / %6d -> %5.1f%%\n", $1, $2, 100.0*$1/$2}'
-echo "-----------------------------------------------"
+echo "---------------------------------------------------"
+passed="`wc -l results/*/PASS | tail -n 1 | awk '{print $1}'`"
+failed="`wc -l results/*/FAIL | tail -n 1 | awk '{print $1}'`"
+crashed="`wc -l results/*/CRASH | tail -n 1 | awk '{print $1}'`"
+timedout="`wc -l results/*/TIMEOUT | tail -n 1 | awk '{print $1}'`"
+warned="`wc -l results/*/WARN | tail -n 1 | awk '{print $1}'`"
+unsupported="`wc -l results/*/NOSUPPORT | tail -n 1 | awk '{print $1}'`"
+total=$[passed+failed+crashed+timedout+warned+unsupported]
+echo "Total tests passed,$passed,$total" | awk -F ',' "$print_line"
+echo "Total tests failed,$failed,$total" | awk -F ',' "$print_line"
+echo "Total tests crashed,$crashed,$total" | awk -F ',' "$print_line"
+echo "Total tests timedout,$timedout,$total" | awk -F ',' "$print_line"
+echo "Total tests warned,$warned,$total" | awk -F ',' "$print_line"
+echo "Total tests unsupported,$unsupported,$total" | awk -F ',' "$print_line"
+echo "---------------------------------------------------"
+echo "Supported tests passed,$passed,$[total-unsupported]" | awk -F ',' "$print_line"
+echo "---------------------------------------------------"
